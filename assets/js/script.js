a421841e8080dafcf333adfc27c9d186e6939926
Rasheed124 <rashdevatrealglobe.com>
2022-12-23 11:19:52 +0100

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const navBurger = document.querySelector(".navBurger");
+const Navmenu = document.querySelector(".Navmenu");
+const menuEffect  = document.querySelector(".menu-effect");
+
+navBurger.addEventListener("click", (e)  => {
+
+	// Selecting burgers and toggling a class
+	const burgers = navBurger.children;
+	Array.from(burgers)
+	    .forEach( burger =>{
+			burger.classList.toggle("hidden")
+		});
+
+
+
+	if (Navmenu.classList.contains("showMenu")) {
+		Navmenu.classList.remove("showMenu");
+
+	  } else {
+		Navmenu.classList.add("showMenu");
+	
+	  }
+
+
+	
+
+
+
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+/*----------------------------------------------------*/
+	/*	Testimonial owlCarousel
+    /*----------------------------------------------------*/
+	let tSlide = $(".testimonial-slide");
+	tSlide.owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+        margin: 40,
+		dots: false,
+		nav: true,
+        navText: ["<div class='nav-button owl-prev '>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		smartSpeed: 1000,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			767: {
+				items: 1,
+			},
+			768: {
+				items: 1,
+			},
+			991: {
+				items: 1,
+			},
+			1000: {
+				items: 1,
+			},
+		},
+	});
+
+        
+});
