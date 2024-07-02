94e2d8451aaca7c123aa5aed9bf6c61a3274f49a
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-07-02 18:22:18 +0100

    content upload

--- app/components/site/Navbar.tsx
+++ app/components/site/Navbar.tsx
@@ -240,7 +240,7 @@ const Navbar = () => {
                     </motion.li>
                   </Link>
                 </ul>
-                <a href="/assets/noor_resume.pdf" target="_blank">
+                <a href="https://flowcv.com/resume/ak4qv6c85v" target="_blank">
                   <motion.button
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
